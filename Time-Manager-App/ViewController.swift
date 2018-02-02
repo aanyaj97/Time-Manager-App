20bb60859604839eb6f034e0361ed8dc087dd16e
Aanya Jhaveri <aanya@aanyas-macbook-pro.local>
2018-02-02 13:43:49 -0600

    Initial Commit

---
+++ Time-Manager-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time-Manager-App
+//
+//  Created by Aanya Jhaveri on 2/2/18.
+//  Copyright © 2018 Aanya Jhaveri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
